8828d810a66db9c1e9fc1cf04897c1c0c7c48e25
Mohamed Ahmad <mobooya@gmail.com>
2012-05-24 20:43:59 -0700

    this time i did git add realtwitter/. instead of git add .

---
+++ realtwitter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Realtwitter::Application.config.secret_token = '73aa8105b26e3ca8631e2f3fe101e866e5e24333e84944a97c35aba86f3aa6e0018dcbc166d3fc3b0bc5bba9ae92dcf2f99fad0a82d9e732138de09e67e95d83'
