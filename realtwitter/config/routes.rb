671f0fb76b861e3c3e6cb5d9e07f23652f691ef5
Mohamed Ahmad <mobooya@gmail.com>
2012-05-27 03:52:10 -0700

    finished layout and routes

--- realtwitter/config/routes.rb
+++ realtwitter/config/routes.rb
@@ -1,6 +1,11 @@
 Realtwitter::Application.routes.draw do
+  get "users/new"
+
   #get 'static_pages/home'
   root :to => 'static_pages#home'
+
+  match '/signup', to: 'users#new'
+
   match '/help', to: 'static_pages#help'
 
   match '/about', to: 'static_pages#about'
